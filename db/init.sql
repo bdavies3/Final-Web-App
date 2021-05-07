472695ffbebb1e1e507f50c9fa7dc0f502c400b5
bdavies3 <78392110+bdavies3@users.noreply.github.com>
2021-05-06 22:48:00 -0400

    Feature: edits on tabs

--- db/init.sql
+++ db/init.sql
@@ -2,13 +2,15 @@ CREATE DATABASE citiesData;
 use citiesData;
 
 CREATE TABLE IF NOT EXISTS tblCitiesImport (
+     `id` int AUTO_INCREMENT,
     `fldName` VARCHAR(21) CHARACTER SET utf8,
     `fldLat` NUMERIC(6, 4),
     `fldLong` NUMERIC(7, 4),
     `fldCountry` VARCHAR(19) CHARACTER SET utf8,
     `fldAbreviation` VARCHAR(3) CHARACTER SET utf8,
     `fldCapitalStatus` VARCHAR(7) CHARACTER SET utf8,
-    `fldPopulation` INT
+    `fldPopulation` INT,
+     PRIMARY KEY (`id`)
 );
 INSERT INTO tblCitiesImport VALUES
     ('Tokyo',35.685,139.7514,'Japan','JPN','primary',35676000),
